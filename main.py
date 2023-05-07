2ddb8cbeab8fa7af09581d6ab94043bc6e41e433
Jakub Dębosz <56394099+Doseeey@users.noreply.github.com>
2023-05-07 12:20:19 +0200

    Added redblack tree w/ insert, search and print function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from bst import BST
+from redblack import RedBlackTree
 from random import randint
 
 def values_list(size):
     values = []
     while len(values) != size:
-        val = randint(0, size)
+        val = randint(1, size)
         if val not in values:
             values.append(val)
     return values
@@ -15,13 +16,25 @@ def fill_tree(tree, size):
         tree.insert(values[i])
 
 if __name__ == "__main__":
-    t1 = BST()
-    fill_tree(t1, 50)
+    # t1 = BST()
+    # fill_tree(t1, 50)
 
-    t1.print_tree()
+    # t1.print_tree()
+
+    # lookup_number = 9
+    # found = t1.search(lookup_number)
+    # if found[0]:
+    #     print(f"Node with value {lookup_number} found after {found[1]} steps.")
+    # else:
+    #     print(f"Node with value {lookup_number} not found.")
+    
+    t2 = RedBlackTree()
+    fill_tree(t2, 10)
+
+    t2.print_tree()
 
     lookup_number = 9
-    found = t1.search(lookup_number)
+    found = t2.search(lookup_number)
     if found[0]:
         print(f"Node with value {lookup_number} found after {found[1]} steps.")
     else:
---
+++ redblack.py
@@ -0,0 +1,216 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+        #1/0 Red/Black
+        self.color = 1
+
+class RedBlackTree:
+    def __init__(self):
+        self.nil = Node(0)
+        self.nil.color = 0
+        self.root = self.nil
+
+    def insert(self, val):
+        node = Node(val)
+        node.left = self.nil
+        node.right = self.nil
+        node.parent = None
+
+        if self.root == self.nil:
+            self.root = node
+            node.color = 0
+        else:
+            curr = self.root
+            while curr != self.nil:
+                node.parent = curr
+                if node.val < curr.val:
+                    curr = curr.left
+                else:
+                    curr = curr.right
+
+            node.color = 1
+            node.parent = node.parent
+
+            if node.parent == None:
+                self.root = node
+            elif node.val < node.parent.val:
+                node.parent.left = node
+            else:
+                node.parent.right = node
+
+            self._insert(node)
+
+    def _insert(self, node):
+        while node.parent.color: #while red
+            if node.parent == node.parent.parent.left:
+                uncle = node.parent.parent.left
+                if uncle.color:
+                    uncle.color = 0
+                    node.parent.color = 0
+                    node.parent.parent.color = 1
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.left:
+                        node = node.parent
+                        self.right_rotate(node)
+                    node.parent.color = 0
+                    node.parent.parent.color = 1
+                    self.left_rotate(node.parent.parent)
+            else:
+                uncle = node.parent.parent.right
+
+                if uncle.color == 1:
+                    uncle.color = 0
+                    node.parent.color = 0
+                    node.parent.parent.color = 1
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.right:
+                        node = node.parent
+                        self.left_rotate(node)
+                    node.parent.color = 0
+                    node.parent.parent.color = 1
+                    self.right_rotate(node.parent.parent)
+                    
+            if node == self.root:
+                break
+
+        self.root.color = 0
+
+    def left_rotate(self, node):
+        edon = node.right
+        node.right = edon.left
+        
+        if edon.left != self.nil:
+            edon.left.parent = node
+        
+        edon.parent = node.parent
+        if node.parent == None:
+            self.root = edon
+        elif node == node.parent.left:
+            node.parent.left = edon
+        else:
+            node.parent.right = edon
+        
+        edon.left = node
+        node.parent = edon
+
+    def right_rotate(self, node):
+        edon = node.left
+        node.left = edon.right
+
+        if edon.right != self.nil:
+            self.root = edon
+        
+        edon.parent = node.parent
+        if node.parent == None:
+            self.root = edon
+        elif node == node.parent.right:
+            node.parent.right = edon
+        else:
+            node.parent.left = edon
+        
+        edon.right = node
+        node.parent = edon
+
+    def search(self, val):
+        steps = 0
+        return self._search(val, steps)
+    
+    def _search(self, val, steps):
+        curr = self.root
+        steps += 1
+        while curr != self.nil and val != curr.val:
+            if val < curr.val:
+                curr = curr.left
+                steps += 1
+            else:
+                curr = curr.right
+                steps += 1
+        
+        if curr == self.nil:
+            return False, steps
+        else:
+            return True, steps
+    
+    # def print_tree(self):
+    #     self._print_tree(self.root)
+
+    # def _print_tree(self, node, level=0):
+    #     if node != self.nil:
+    #         indent = "    " * level
+    #         color = "R" if node.color == 1 else "B"
+    #         print(f"{indent}{color}: {node.val}")
+    #         self._print_tree(node.left, level + 1)
+    #         self._print_tree(node.right, level + 1)
+
+    def print_tree(self):
+        lines, *_ = self._print_tree(self.root)
+        print("================= Red Black Tree =================")
+        for line in lines:
+            print(line)
+        print("==================================================")
+
+    def _print_tree(self, node):
+        # No wings.
+        if node.right is None and node.left is None:
+            color = "R" if node.color == 1 else "B"
+            if node.val:    
+                line = f'{color}: {node.val}'
+            else:
+                line = f'NIL'
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # No right wing.
+        if node.right is None:
+            lines, n, p, x = self._print_tree(node.left)
+            color = "R" if node.color == 1 else "B"
+            if node.val:    
+                s = f'{color}: {node.val}'
+            else:
+                s = f'NIL'
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # No left wing.
+        if node.left is None:
+            lines, n, p, x = self._print_tree(node.right)
+            color = "R" if node.color == 1 else "B"
+            if node.val:    
+                s = f'{color}: {node.val}'
+            else:
+                s = f'NIL'
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Both wings.
+        left, n, p, x = self._print_tree(node.left)
+        right, m, q, y = self._print_tree(node.right)
+        color = "R" if node.color == 1 else "B"
+        if node.val:    
+                s = f'{color}: {node.val}'
+        else:
+            s = f'NIL'
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+    
