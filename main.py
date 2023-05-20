7132168338c8b22492b250e90add46fedecda6b2
Jakub Dębosz <56394099+Doseeey@users.noreply.github.com>
2023-05-20 17:04:17 +0200

    added helper in main for analysis

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bst import BST
 from redblack import RedBlackTree
 from avl import AVLTree
 from random import randint
+from datetime import datetime
 
 def values_list(size):
     values = []
@@ -16,10 +17,32 @@ def fill_tree(tree, size):
     for i in range(size-1):
         tree.insert(values[i])
 
-if __name__ == "__main__":
-    # t1 = BST()
-    # fill_tree(t1, 50)
+def fill_time_comparision(type):
+    print(f"\nInsertion times for {type}:")
+    for size in [1000, 5000, 10000, 20000, 30000, 50000]:
+        start = datetime.now()
+        if type == "BST":
+            tree = BST()
+        elif type == "RedBlack":
+            tree = RedBlackTree()
+        elif type == "AVL":
+            tree = AVLTree()
+        else: 
+            print("Wrong type, choose correct type: BST, RedBlack, AVL")
+            return
+        
+        fill_tree(tree, size)
+        stop = datetime.now()
 
+        print(f" - Inserting {size} elements took {stop - start} time.")
+    return
+
+if __name__ == "__main__":
+    t1 = BST()
+    fill_tree(t1, 50)
+    fill_time_comparision("BST")
+    fill_time_comparision("RedBlack")
+    fill_time_comparision("AVL")
     # t1.print_tree()
 
     # lookup_number = 9
@@ -45,8 +68,8 @@ if __name__ == "__main__":
     # else:
     #     print(f"Node with value {lookup_number} not found.")
 
-    t3 = AVLTree()
-    fill_tree(t3, 10)
-    t3.print_tree()
-    z = t3.search(8)
-    print(z[1])
+    # t3 = AVLTree()
+    # fill_tree(t3, 10)
+    # t3.print_tree()
+    # z = t3.search(8)
+    # print(z[1])
